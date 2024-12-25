84c665e2295110e75ebe1aefc032af91116f5c6a
AeZG <gregorydyuldin@gmail.com>
2024-12-25 19:01:04 +0300

    Add description for TwoWordName.go

--- TwoWordName/TwoWordName.go
+++ TwoWordName/TwoWordName.go
@@ -1,12 +1,22 @@
+/*Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+patrick feeney => P.F*/
+
 package TwoWordName
 
 import (
-  "fmt"
-  "strings"
+	"fmt"
+	"strings"
 )
 
 func AbbrevName(name string) string {
-  splitName := strings.Split(name, " ")
-  initial := strings.ToUpper(fmt.Sprintf("%s.%s", splitName[0][:1], splitName[1][:1]))
-  return initial
+	splitName := strings.Split(name, " ")
+	initial := strings.ToUpper(fmt.Sprintf("%s.%s", splitName[0][:1], splitName[1][:1]))
+	return initial
 }
